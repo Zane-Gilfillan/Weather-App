83aa48c65468acc3b0c4074c61fb2af25410655f
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-03-14 15:30:04 -0500

    setting up variable ideas for API

--- js/weather-data.js
+++ js/weather-data.js
@@ -1,7 +1,23 @@
+let cityName = $('#city')
+let cityTemp = $('#temp')
+let cityHigh = $('#high')
+let cityLow = $('#low')
+
 $.getJSON(
     'http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&APPID=8979032860447814896d5eb119d2c288',
     function(data) {
         console.log(data)
-        console.log(data.name)
-        console.log(data.main.temp)
-    })
+        nameId = data.name
+        tempId = data.main.temp
+        highId = data.main.temp_max
+        lowId = data.main.temp_min
+
+        cityName.text(nameId)
+        cityTemp.text(tempId)
+        cityHigh.text(highId)
+        cityLow.text(lowId)
+
+        console.log(nameId, tempId, highId, lowId)
+        
+    }    
+)
