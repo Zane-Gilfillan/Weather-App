712cb671b8ec8dad0723c46a2953ae50e21ee604
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-03-15 16:16:54 -0500

    setting up styling idea for five-day

---
+++ js/weather-icons.js
@@ -0,0 +1,16 @@
+let skycons = new Skycons({"color": "pink"});
+  
+  // you can add a canvas by it's ID...
+  skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
+
+  // ...or by the canvas DOM element itself.
+  skycons.add(document.getElementById("icon2"), Skycons.RAIN);
+
+  // start animation!
+  skycons.play();
+
+  // want to change the icon? no problem:
+  skycons.set("icon1", Skycons.PARTLY_CLOUDY_NIGHT);
+
+  // want to remove one altogether? no problem:
+  skycons.remove("icon2");
